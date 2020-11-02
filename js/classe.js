ea1afe9bafbc666d5906842b1a47d3f2bab58ed3
Nathan <nathan.hutin@cpe.fr>
2020-11-02 17:21:49 +0100

    openweathermap fonctionnel

--- js/classe.js
+++ js/classe.js
@@ -1,3 +1,5 @@
+
+
 class Voyage {
     constructor(ville,prix,image,enfant,repas,animaux,idd){
         this._ville=ville;
@@ -13,7 +15,9 @@ class Voyage {
     set(meteo){
         this._weather=meteo;
     }
-}
+};
+
+
 
 var nb1=new Voyage(
     ville="Monaco",
@@ -35,14 +39,26 @@ var nb2=new Voyage(
     idd=2
 )
 
-const lvoyage=[nb1,nb2];
-
 function weather(voyage){
     const key="4c280b90ff25b4fbd57d770d12f45694";
     var url="https://api.openweathermap.org/data/2.5/weather?q="+voyage._ville+"&units=metric&appid="+key;
     fetch(url).then(function(resp) { return resp.json()}).then(function(data){
-        voyage.set(data.main.temp)});
+        voyage.set(data.main.temp);
+        console.log(voyage);   
+        div1.innerHTML += `<div class="affvoyage">
+        <a href="formulaire.html"><img src="`+voyage._image+`" alt="Photo"`+voyage._ville+`></a>
+        <ul>
+            <li>`+voyage._ville+`</li>
+            <li>prix`+voyage._prix+`</li>
+            <li>enfant `+voyage._enfant+`</li>
+            <li>repas du voyage `+voyage._repas+`</li>
+            <li>animaux `+voyage._animaux+`</li>
+            <li>température`+voyage._weather+`°C<li>
+        </ul> 
+    </div>`});
 };
+
+const lvoyage=[nb1,nb2];
 
 var div1 = document.getElementsByClassName("toutvoyage")[0];
 
@@ -50,20 +66,6 @@ for (i of lvoyage){
     weather(i)
 };
 
-for (i of lvoyage){
-    console.log(i);   
-    div1.innerHTML += `<div class="affvoyage">
-    <a href="formulaire.html"><img src="`+i._image+`" alt="Photo"`+i._ville+`></a>
-    <ul>
-        <li>`+i._ville+`</li>
-        <li>prix`+i._prix+`</li>
-        <li>enfant `+i._enfant+`</li>
-        <li>repas du voyage `+i._repas+`</li>
-        <li>animaux `+i._animaux+`</li>
-        <li>température`+i._weather+`<li>
-    </ul> 
-</div>`
-};
 
 
 //.the;n(function(data){
