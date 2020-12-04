9df6dbbdf77a4587e35f7810281872c80b277588
hutin <hutin.nathan1@gmail.com>
2020-12-04 16:50:18 +0100

    change le prix

--- js/formulaire.js
+++ js/formulaire.js
@@ -1,4 +1,17 @@
-var x = document.getElementById("nb_adulte").value
-var y = document.getElementById("nb_enfant").value
+var x = document.getElementById("nb_adulte").value;
+var y = document.getElementById("nb_enfant").value;
 
-prix = x*200+y*200*0.4
+prix = x*200+y*20*0.4;
+
+function affiche(){
+    var x = document.getElementById("nb_adulte").value;
+    var y = document.getElementById("nb_enfant").value;
+
+    prix = x*200+y*20*0.4;
+    return prix
+};
+
+
+
+
+console.log("oui");
